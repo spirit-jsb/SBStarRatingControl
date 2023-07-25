3bb249a91b8b0fbb3f1c7202bdd397e0c3441ad6
Max <sibo_jian_29903549@163.com>
2023-07-25 14:25:21 +0800

    Add calculateFillLevel(for:with:) function in SBRatingHandler.swift

---
+++ Sources/Utils/SBRatingHandler.swift
@@ -0,0 +1,36 @@
+//
+//  SBRatingHandler.swift
+//  SBStarRatingControl
+//
+//  Created by JONO-Jsb on 2023/7/25.
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+struct SBRatingHandler {
+    static func calculateFillLevel(for rating: Float, with fillMode: SBStarRatingControl.Configuration.FillMode) -> Float {
+        var result = rating
+
+        if result > 1.0 {
+            result = 1.0
+        }
+        if result < 0.0 {
+            result = 0.0
+        }
+
+        switch fillMode {
+            case .full:
+                result = round(result)
+            case .half:
+                result = round(result * 2.0) / 2.0
+            case .precise:
+                break
+        }
+
+        return result
+    }
+}
+
+#endif
