57c8da8e906bcd9a9a8dad1587490d40c1a003c5
Max <sibo_jian_29903549@163.com>
2023-07-25 08:16:53 +0800

    Initial SBStarRatingControl project

---
+++ Demo/Demo/Spirit-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SBStarRatingControl-Demo
+//
+//  Created by JONO-Jsb on 2023/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
