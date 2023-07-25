196d8dde70895e270fb6291ce9ae14be54147b03
Max <sibo_jian_29903549@163.com>
2023-07-25 08:41:33 +0800

    Add SBStarRatingControl.swift file

---
+++ Sources/Core/SBStarRatingControl.swift
@@ -0,0 +1,20 @@
+//
+//  SBStarRatingControl.swift
+//  SBStarRatingControl
+//
+//  Created by JONO-Jsb on 2023/7/25.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+public class SBStarRatingControl: UIView {
+    // // Only override draw() if you perform custom drawing.
+    // // An empty implementation adversely affects performance during animation.
+    // override func draw(_ rect: CGRect) {
+    //    // Drawing code
+    // }
+}
+
+#endif
