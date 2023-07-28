4a54c2be1083bbac93a1f3ed09d1f2d36e3a6bc4
Max <sibo_jian_29903549@163.com>
2023-07-28 15:15:14 +0800

    Import SBStarRatingControl.framework on SBStarRatingControl-Demo target

--- Demo/Demo/Spirit-Demo/ViewController.swift
+++ Demo/Demo/Spirit-Demo/ViewController.swift
@@ -5,15 +5,12 @@
 //  Created by JONO-Jsb on 2023/7/25.
 //
 
+import SBStarRatingControl
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
