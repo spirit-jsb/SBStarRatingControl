7e746782f1c98a4c066b55141de369486156ccca
Max <sibo_jian_29903549@163.com>
2023-08-01 17:07:33 +0800

    Add image star and list rating example

---
+++ Demo/Demo/Spirit-Demo/ListRatingTableViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  ListRatingTableViewCell.swift
+//  SBStarRatingControl-Demo
+//
+//  Created by JONO-Jsb on 2023/8/1.
+//
+
+import SBStarRatingControl
+import UIKit
+
+class RatingListTableViewCell: UITableViewCell {
+    lazy var preciseStarRatingControl: SBStarRatingControl = {
+        let starRatingControl = SBStarRatingControl()
+        starRatingControl.translatesAutoresizingMaskIntoConstraints = false
+        starRatingControl.configuration.fillMode = .precise
+        starRatingControl.configuration.filledBackgroundColor = UIColor(red: 0.0, green: CGFloat(47) / 255.0, blue: CGFloat(167) / 255.0, alpha: 1.0)
+        starRatingControl.configuration.emptyBorderColor = UIColor(red: 0.0, green: CGFloat(47) / 255.0, blue: CGFloat(167) / 255.0, alpha: 1.0)
+        starRatingControl.configuration.filledBorderColor = UIColor(red: 0.0, green: CGFloat(47) / 255.0, blue: CGFloat(167) / 255.0, alpha: 1.0)
+        starRatingControl.configuration.blockingTouch = true
+        return starRatingControl
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        self.constructViewHierarchy()
+        self.activateConstraints()
+    }
+
+    override func prepareForReuse() {
+        self.preciseStarRatingControl.prepareForReuse()
+    }
+
+    func updateRating(_ rating: Float) {
+        self.preciseStarRatingControl.rating = rating
+    }
+
+    private func constructViewHierarchy() {
+        self.contentView.addSubview(self.preciseStarRatingControl)
+    }
+
+    private func activateConstraints() {
+        self.preciseStarRatingControl.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
+        self.preciseStarRatingControl.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+    }
+}
